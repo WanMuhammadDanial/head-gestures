787198f7ae054ed4b4954e32b39b3ccb1de2e88d
dysrexn <58846727+dysrexn@users.noreply.github.com>
2021-12-05 22:40:03 +0800

    Update TrainStuff.py
    - Removed a layer in training //not too sure if this actually affected anything, was testing out stuff and this seems to be the best way to get high accuracy/low loss
    - Changed epochs to be 250, might consider making it lower but 250 seems to be an optimal amount
    - Added optimizer, not fully sure what this does exactly
    - Added 'tilt left' and 'tilt right'

--- TrainStuff.py
+++ TrainStuff.py
@@ -1,5 +1,6 @@
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.optimizers import SGD
 import numpy as np
 import os
 from tensorflow.keras.models import Sequential
@@ -9,7 +10,7 @@ from tensorflow.keras.callbacks import TensorBoard
 DATA_PATH = os.path.join('MP_Data')
 
 # plug in all the gestures you want in actions
-actions = np.array(['neutral', 'up', 'down', 'left', 'right'])
+actions = np.array(['neutral', 'up', 'down', 'left', 'right', 'tilt left', 'tilt right'])
 no_sequences = 30
 sequence_length = 30
 label_map = {label: num for num, label in enumerate(actions)}
@@ -28,18 +29,20 @@ x = np.array(sequences)
 y = to_categorical(labels).astype(int)
 print(x.shape)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.05)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
 
 model = Sequential()
 model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30, 1536)))
-model.add(LSTM(128, return_sequences=True, activation='relu'))
 model.add(LSTM(64, return_sequences=False, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(actions.shape[0], activation='softmax'))
+opt = SGD(lr=0.01, momentum=0.9)
+model.compile(loss='mean_absolute_error', optimizer=opt, metrics=['mse'])
+#https://machinelearningmastery.com/how-to-choose-loss-functions-when-training-deep-learning-neural-networks/
 
 # not sure if this matters but I usually delete head_gesture.h5 every time before re-training
 model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-model.fit(x_train, y_train, epochs=1000)
+model.fit(x_train, y_train, epochs=250)
 model.summary()
 model.save('head_gesture.h5')
