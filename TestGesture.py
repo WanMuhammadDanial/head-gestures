965dcc8f313f32dcaadcd4e964937074f82b502f
Wan Muhammad Danial Bin Seniwan <danial.seniwan@tmrnd.com.my>
2023-05-20 14:17:22 +0800

    dependency fix 2

--- TestGesture.py
+++ TestGesture.py
@@ -20,7 +20,7 @@ def mediapipe_detection(med_image, model_pipe):
 
 
 def draw_landmarks(draw_image, draw_results):
-    mp_drawing.draw_landmarks(draw_image, draw_results.face_landmarks, mp_holistic.FACE_CONNECTIONS,
+    mp_drawing.draw_landmarks(draw_image, draw_results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
                               mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
                               mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1))
 
